a90f49386495d499f76f86c97a2df2f90a17927c
wangchristine <c56984200123@gmail.com>
2025-02-25 17:16:30 +0800

    update chat

--- src/components/Lottery/LotteryChat.vue
+++ src/components/Lottery/LotteryChat.vue
@@ -1,14 +1,72 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const currentMessage = ref("");
+const messages = ref([
+  { text: "test1", isMe: true },
+  { text: "test2", isMe: false },
+]);
+
+const sendMessage = () => {
+  if (currentMessage.value) {
+    messages.value.push({ text: currentMessage.value, isMe: true });
+    currentMessage.value = "";
+  }
+};
+</script>
 
 <template>
+  <div class="chat-block">
+    <v-row v-for="(message, index) in messages" :key="index" justify="center">
+      <v-col cols="12" :class="message.isMe ? 'text-right' : 'text-left'">
+        <v-card :class="message.isMe ? 'my-message' : 'ai-message'" class="d-inline-block">
+          <v-card-text>
+            {{ message.text }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
   <v-row class="align-center no-wrap">
     <v-col class="flex-grow-1">
-      <v-textarea class="py-2" label="Please Type..." variant="outlined" rows="5" no-resize clearable></v-textarea>
+      <v-textarea
+        v-model="currentMessage"
+        class="py-2"
+        label="Please Type..."
+        variant="outlined"
+        rows="5"
+        no-resize
+        clearable
+      ></v-textarea>
     </v-col>
     <v-col class="flex-shrink-0" cols="auto">
-      <v-btn color="deep-purple-lighten-3" icon="mdi-send" size="small"></v-btn>
+      <v-btn color="deep-purple-lighten-3" icon="mdi-send" size="small" @click="sendMessage"></v-btn>
     </v-col>
   </v-row>
 </template>
 
-<style scoped></style>
+<style scoped>
+.my-message {
+  background-color: #b39ddb;
+  color: white;
+  border-radius: 24px;
+  max-width: 60%;
+  text-align: left;
+}
+
+.ai-message {
+  max-width: 100%;
+  text-align: left;
+}
+
+.chat-block {
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  padding: 10px;
+}
+
+.v-card-text {
+  padding: 10px 20px;
+}
+</style>
--- src/views/Lottery/Create.vue
+++ src/views/Lottery/Create.vue
@@ -11,27 +11,23 @@ const tab = ref(null);
     <v-app-bar-title>Lottery Create</v-app-bar-title>
   </v-app-bar>
   <v-container class="py-8 px-6" fluid>
-    <v-row>
-      <v-col cols="12">
-        <v-card>
-          <v-tabs v-model="tab" align-tabs="center" color="deep-purple-lighten-3" grow>
-            <v-tab value="chat">Chat</v-tab>
-            <v-tab value="form">Form</v-tab>
-          </v-tabs>
+    <v-card>
+      <v-tabs v-model="tab" align-tabs="center" color="deep-purple-lighten-3" grow>
+        <v-tab value="chat">Chat</v-tab>
+        <v-tab value="form">Form</v-tab>
+      </v-tabs>
 
-          <v-card-text>
-            <v-carousel v-model="tab" :show-arrows="false" :continuous="false" hide-delimiters style="height: 100%">
-              <v-carousel-item value="chat">
-                <LotteryChat />
-              </v-carousel-item>
-              <v-carousel-item value="form">
-                <LotteryForm />
-              </v-carousel-item>
-            </v-carousel>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-card-text>
+        <v-carousel v-model="tab" :show-arrows="false" :continuous="false" hide-delimiters style="height: 100%">
+          <v-carousel-item value="chat">
+            <LotteryChat />
+          </v-carousel-item>
+          <v-carousel-item value="form">
+            <LotteryForm />
+          </v-carousel-item>
+        </v-carousel>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
